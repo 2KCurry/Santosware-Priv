4a1e73eb19d0ade440b897be86296057c9b4c5d6
2KCurry <52842919+2KCurry@users.noreply.github.com>
2021-12-16 00:47:22 -0600

    Update get_localplayer.lua

--- getters/get_localplayer.lua
+++ getters/get_localplayer.lua
@@ -1,15 +1,15 @@
-local gun_system
+local localplayer
 
 for _, v in next, getgc(true) do
     if type(v) == "table" then
-        if rawget(v, "setsprintdisable") then
-            gun_system = v
+        if rawget(v, "setbasewalkspeed") then
+            localplayer = v
         end
     end
 end
 
-if not gun_system then
-    return error("failed to find gun_system")
+if not localplayer then
+    return error("failed to find localplayer")
 end
 
-getgenv().gun_system = gun_system
+getgenv().localplayer = localplayer
